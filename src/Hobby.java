632f96b78bf241d8ffb2d03aa87e85618df7da8f
dirtybits <dirtybitsofficial@gmail.com>
2017-11-19 02:10:31 -0600

    Hobby

--- src/Hobby.java
+++ src/Hobby.java
@@ -3,12 +3,22 @@ import java.time.Duration;
 public class Hobby {
 
     private String _hobbyTitle;
+    private boolean _isRunning;
     private Duration _timeSpent;
 
 
-    public Hobby(Duration _timeSpent, String _hobbyTitle) {
+    public Hobby(Duration _timeSpent, String _hobbyTitle, boolean _isRunning) {
         this._timeSpent = _timeSpent;
         this._hobbyTitle = _hobbyTitle;
+        this._isRunning = _isRunning;
+    }
+
+    public boolean is_isRunning() {
+        return _isRunning;
+    }
+
+    public void set_isRunning(boolean _isRunning) {
+        this._isRunning = _isRunning;
     }
 
     public Duration get_timeSpent() {
@@ -26,4 +36,6 @@ public class Hobby {
     public void set_hobbyTitle(String _hobbyTitle) {
         this._hobbyTitle = _hobbyTitle;
     }
+
+
 }
---
+++ src/HobbyManager.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class HobbyManager {
+
+    private ArrayList<Hobby> hobbyList = new ArrayList<>();
+
+    public HobbyManager(ArrayList<Hobby> hobbyList) {
+        this.hobbyList = hobbyList;
+    }
+
+
+}
